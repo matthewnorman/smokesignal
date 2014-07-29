2e3dd61c9ee793464043572f91bc9b058b42821a
Matthew Norman <mnorman@Workshop.workshop>
2014-07-28 20:29:40 -0700

    Add basic test for RSA key encryption

---
+++ tests/test_encrypter.py
@@ -0,0 +1,23 @@
+import pytest
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from smokesignal import encrypter
+
+
+def test_basic_encryption():
+    """
+    Generate a simple RSA key and make sure that you
+    can encrypt and decrypt context
+
+    """
+    crypter = encrypter.Crypter()
+
+    content = 'This is a short string'
+
+    private_key = rsa.generate_private_key(public_exponent=65537,
+                                           key_size=2048,
+                                           backend=default_backend())
+
+    public_key = private_key.public_key()
+    print crypter.encrypt(key=public_key, content=content)
