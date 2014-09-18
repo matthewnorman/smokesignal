0a3ad30d04dae010cf29a3f92e0c85bffcfd023d
Matthew Norman <none>
2014-09-17 20:19:30 -0700

    Enable dual-mode encryption with Fernet

--- smokesignal/encrypter.py
+++ smokesignal/encrypter.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives import asymmetric
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.serialization import load_pem_pkcs8_private_key
+from cryptography.fernet import Fernet
 
 class Crypter(object):
     """
@@ -12,15 +13,47 @@ class Crypter(object):
 
     """
 
-    def __init__(self):
+    def __init__(self, key_size=1024):
         super(Crypter, self).__init__()
         self.padding = asymmetric.padding.OAEP(
             mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA1()),
             algorithm=hashes.SHA1(),
             label=None)
+        self.key_size=1024
+
+    def encrypt_fernet(self, public_key, content):
+        """
+        Fernet entrypoint, take a public key from a remote source, and use
+        it to symmetrically encrypt content using an underlying symmetric
+        key, which is then returned.
+
+        This is for testing dual-mode symmetric-asymmetric encryption. It
+        is far too weak for actual use.
+
+        return signature should be: encrypted_key, encrypted_content
+        """
+        fernet_key = Fernet.generate_key()
+        fernet_crypter = Fernet(key=fernet_key)
+        encrypted_content = fernet_crypter.encrypt(bytes(content))
+        encrypted_key = self.encrypt_rsa(public_key=public_key,
+                                         content=fernet_key)
+        return encrypted_key, encrypted_content
+
+    def decrypt_fernet(self, private_key, encrypted_key, ciphertext):
+        """
+        Take a generated key that was encrypted via the crytography
+        fernet mechanism and use to unpack the ciphertext.
+
+        """
+
+        key = self.decrypt_rsa(private_key=private_key,
+                               ciphertext=encrypted_key)
+        fernet_crypter = Fernet(key=key)
+        return fernet_crypter.decrypt(ciphertext)
+        
 
 
-    def encrypt(self, public_key, content):
+    def encrypt_rsa(self, public_key, content):
         """
         Basic wrapper. Given a key as an object, and a piece of content,
         encrypt it.
@@ -30,7 +63,7 @@ class Crypter(object):
         ciphertext = public_key.encrypt(content, self.padding)
         return ciphertext
 
-    def decrypt(self, private_key, ciphertext):
+    def decrypt_rsa(self, private_key, ciphertext):
         """
         Basic wrapper. Given a key as an object, return the actual content
         of a ciphertext.
--- tests/test_encrypter.py
+++ tests/test_encrypter.py
@@ -20,6 +20,30 @@ def test_basic_encryption():
                                            backend=default_backend())
 
     public_key = private_key.public_key()
-    ciphertext = crypter.encrypt(public_key=public_key, content=content)
-    result = crypter.decrypt(private_key=private_key, ciphertext=ciphertext)
+    ciphertext = crypter.encrypt_rsa(public_key=public_key, content=content)
+    result = crypter.decrypt_rsa(private_key=private_key,
+                                 ciphertext=ciphertext)
     assert content == result
+
+
+def test_fernet_encryption():
+    """
+    A short test of dual-mode encryption
+
+    """
+
+    content = 'This is a short string'
+    crypter = encrypter.Crypter()
+
+    private_key = rsa.generate_private_key(public_exponent=65537,
+                                           key_size=2048,
+                                           backend=default_backend())
+
+    public_key = private_key.public_key()
+
+    key, text = crypter.encrypt_fernet(public_key=public_key,
+                                       content=content)
+    
+    result = crypter.decrypt_fernet(private_key=private_key,
+                                    encrypted_key=key, ciphertext=text)
+    assert result == content
